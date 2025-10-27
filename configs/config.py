84a5952b1454cf77f722d7e2db308893b3899d34
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2025-10-27 13:49:14 -0500

    Fix syntax error in config.py for file existence check

--- configs/config.py
+++ configs/config.py
@@ -85,7 +85,7 @@ def parse_args():
         for dir in dirs:    
             cfg = get_cfg_defaults()
             cfg_file = osp.join(root, dir, 'config.yaml')
-            if osp.exists(cfg_file)
+            if osp.exists(cfg_file):
                 cfg = update_cfg(cfg, cfg_file)
                 cfg.cfg_file = cfg_file
         
